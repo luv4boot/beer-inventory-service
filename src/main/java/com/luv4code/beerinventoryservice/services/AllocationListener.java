6523adc9dd9340878e5767befcf8adbda70443d0
luv4code-lc <madhav.p.akv@gmail.com>
2022-03-04 08:28:19 +0530

    handling allocation requests

---
+++ src/main/java/com/luv4code/beerinventoryservice/services/AllocationListener.java
@@ -0,0 +1,43 @@
+package com.luv4code.beerinventoryservice.services;
+
+import com.lfc.model.events.AllocateOrderRequest;
+import com.lfc.model.events.AllocateOrderResult;
+import com.luv4code.beerinventoryservice.config.JmsConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class AllocationListener {
+    private final AllocationService allocationService;
+    private final JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
+    public void listen(AllocateOrderRequest request){
+        AllocateOrderResult.AllocateOrderResultBuilder builder = AllocateOrderResult.builder();
+        builder.beerOrderDto(request.getBeerOrderDto());
+
+        try{
+            Boolean allocationResult = allocationService.allocateOrder(request.getBeerOrderDto());
+
+            if (allocationResult){
+                builder.pendingInventory(false);
+            } else {
+                builder.pendingInventory(true);
+            }
+
+            builder.allocationError(false);
+        } catch (Exception e){
+            log.error("Allocation failed for Order Id:" + request.getBeerOrderDto().getId());
+            builder.allocationError(true);
+        }
+
+        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE,
+                builder.build());
+
+    }
+}
